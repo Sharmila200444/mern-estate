e20afbee563e23c0e07e0166c8d0a0736b8cd306
Sharmila200444 <sharmilaelumalais@mail.com>
2024-05-24 18:45:29 +0530

    connect to database

--- sharu/src/App.jsx
+++ sharu/src/App.jsx
@@ -4,24 +4,11 @@ import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import About from './pages/About';
 import Profile from './pages/Profile';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import Header from './Components/Header';
-=======
->>>>>>> 49d0357515e6c8b739240a69b184429f4a1c51c1
-=======
->>>>>>> 49d0357515e6c8b739240a69b184429f4a1c51c1
-
 export default function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <Header/>
-=======
->>>>>>> 49d0357515e6c8b739240a69b184429f4a1c51c1
-=======
->>>>>>> 49d0357515e6c8b739240a69b184429f4a1c51c1
+    <Header />
     <Routes>
       <Route path="/" element={<Home />}  />
       <Route path="/Signup" element={<Signup />}  />
